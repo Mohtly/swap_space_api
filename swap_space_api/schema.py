9b6aea0177d7b90621a9097e7cdb0498d53e7131
Nate Thom <39501007+NateThom@users.noreply.github.com>
2019-11-10 00:30:10 -0800

    users and items mutations. (#1)

--- swap_space_api/schema.py
+++ swap_space_api/schema.py
@@ -6,5 +6,7 @@ import users.schema, items.schema
 class Query(items.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(items.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
