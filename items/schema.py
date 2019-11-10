0126c0d5cbf7c09abf823433e6fc9d7661cce0d9
Nathan Thom <nathanthom@nevada.unr.edu>
2019-11-09 22:25:06 -0800

    Comment out mutation code

--- items/schema.py
+++ items/schema.py
@@ -15,33 +15,33 @@ class Query(graphene.ObjectType):
     def resolve_items(self, info, **kwargs):
         return Item.objects.all()
 
-class CreateItem(graphene.Mutation):
-    item_id = graphene.Int()
-    photo = graphene.String()
-    value_low = graphene.Int()
-    value_high = graphene.Int()
-    description = graphene.Int()
-    traded = graphene.Boolean()
-    date_time_created = graphene.types.datetime.DateTime()
-    
-
-    #2
-    class Arguments:
-        url = graphene.String()
-        description = graphene.String()
-
-    #3
-    def mutate(self, info, url, description):
-        link = Link(url=url, description=description)
-        link.save()
-
-        return CreateLink(
-            id=link.id,
-            url=link.url,
-            description=link.description,
-        )
-
-
-#4
-class Mutation(graphene.ObjectType):
-    create_link = CreateLink.Field()
+# class CreateItem(graphene.Mutation):
+#     item_id = graphene.Int()
+#     photo = graphene.String()
+#     value_low = graphene.Int()
+#     value_high = graphene.Int()
+#     description = graphene.Int()
+#     traded = graphene.Boolean()
+#     date_time_created = graphene.types.datetime.DateTime()
+#
+#
+#     #2
+#     class Arguments:
+#         url = graphene.String()
+#         description = graphene.String()
+#
+#     #3
+#     def mutate(self, info, url, description):
+#         link = Link(url=url, description=description)
+#         link.save()
+#
+#         return CreateLink(
+#             id=link.id,
+#             url=link.url,
+#             description=link.description,
+#         )
+#
+#
+# #4
+# class Mutation(graphene.ObjectType):
+#     create_link = CreateLink.Field()
