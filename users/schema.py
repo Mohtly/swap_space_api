858a04e01a2f0cd7a670e43d3e5288071f5ac593
Nathan Thom <nathanthom@nevada.unr.edu>
2019-11-10 00:23:46 -0800

    users and items mutations.

--- users/schema.py
+++ users/schema.py
@@ -14,3 +14,33 @@ class Query(graphene.ObjectType):
 
     def resolve_users(self, info, **kwargs):
         return User.objects.all()
+
+class CreateUser(graphene.Mutation):
+    # user_id = graphene.Int()
+    first_name = graphene.String()
+    last_name = graphene.String()
+    email = graphene.String()
+    # date_time_created = graphene.types.datetime.DateTime()
+
+    #2
+    class Arguments:
+        first_name = graphene.String()
+        last_name = graphene.String()
+        email = graphene.String()
+
+    #3
+    def mutate(self, info, first_name, last_name, email):
+        user = User(first_name=first_name, last_name=last_name, email=email)
+        user.save()
+
+        return CreateUser(
+            # id=link.id,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email
+        )
+
+
+#4
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
